524152045d98a92f4aa4481d63397e3b620251e4
Dan <hornedigital01@gmail.com>
2025-02-27 21:24:09 +0000

    push updates

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Footballde 24/25
+#Footballdle
+
+Clone of the well known game Wordle, but with a twist. Guess the Premier League Footballer in 6 guesses or less.
+
+www.footballdle.co.uk/
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,40 @@
+import {
+  ChartBarIcon,
+  CogIcon,
+  InformationCircleIcon,
+} from '@heroicons/react/outline'
+
+type Props = {
+  setIsInfoModalOpen: (value: boolean) => void
+  setIsStatsModalOpen: (value: boolean) => void
+  setIsSettingsModalOpen: (value: boolean) => void
+}
+
+export const Navbar = ({
+  setIsInfoModalOpen,
+  setIsStatsModalOpen,
+  setIsSettingsModalOpen,
+}: Props) => {
+  return (
+    <div className="navbar">
+      <div className="navbar-content px-5">
+        <p className="text-3xl ml-2.5 mb-2 mt-2 font-bold dark:text-white">Footballdle</p>
+        <div className="right-icons">
+          <InformationCircleIcon
+            className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
+            onClick={() => setIsInfoModalOpen(true)}
+          />
+          <ChartBarIcon
+            className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
+            onClick={() => setIsStatsModalOpen(true)}
+          />
+          <CogIcon
+            className="h-6 w-6 mr-10 cursor-pointer dark:stroke-white"
+            onClick={() => setIsSettingsModalOpen(true)}
+          />
+        </div>
+      </div>
+      <hr></hr>
+    </div>
+  )
+}
