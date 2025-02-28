32834e34d898878213fc9f009ece9dce56ed9561
Dan <hornedigital01@gmail.com>
2025-02-28 19:55:14 +0000

    push it

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -75,8 +75,8 @@ export const localeAwareUpperCase = (text: string) => {
 }
 
 export const getWordOfDay = () => {
-  // January 1, 2022 Game Epoch
-  const epochMs = new Date(2022, 0).valueOf()
+  // January 1, 2025 Game Epoch
+  const epochMs = new Date(2025, 0).valueOf()
   const now = Date.now()
   const msInDay = 86400000
   const index = Math.floor((now - epochMs) / msInDay)
